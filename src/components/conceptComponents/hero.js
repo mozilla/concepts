149728f42975c6899b1dcf6df7d922591b0e8a4b
John Gruen <john.gruen@gmail.com>
2019-02-06 17:26:12 +0100

    add cobranding and a basic 404 page

--- src/components/conceptComponents/hero.js
+++ src/components/conceptComponents/hero.js
@@ -1,12 +1,22 @@
 import React from 'react'
 
-const Hero = ({ hero, surveyUrl }) => {
+const Hero = ({ hero, surveyUrl, cobrand }) => {
   const { title, text, cta, image } = hero[0]
+  console.log(cobrand);
   return (
     <section className="mzp-c-hero mzp-has-image mzp-t-product-firefox">
       <div className="mzp-l-content">
         <div className="mzp-c-hero-body">
           <h1 className="mzp-c-hero-title">{title}</h1>
+          {cobrand !== '' && (
+            <h4 style={{
+              margin: "-10px 0 20px",
+              fontWeight: "400",
+              fontSize: "24px",
+              fontFamily: "Open Sans, X-LocaleSpecific, sans-serif"}}>
+              powered by <b>{cobrand}</b>
+            </h4>
+          )}
           <div className="mzp-c-hero-desc">
             <p>{text}</p>
           </div>
@@ -17,14 +27,12 @@ const Hero = ({ hero, surveyUrl }) => {
             >
               {cta}
             </a>
+            {console.log(cobrand)}
           </p>
         </div>
       </div>
       <div className="mzp-c-hero-image">
-        <img
-          src={image.publicURL}
-          alt={title}
-        />
+        <img src={image.publicURL} alt={title} />
       </div>
     </section>
   )
